1a5e2ffa4c93c1f4566a0408c30111b22d82f219
Manjit Singh <manjitsingh@Randeeps-MacBook-Pro.local>
2017-11-12 01:48:55 -0500

    Initial Commit

---
+++ College/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College
+//
+//  Created by Manjit Singh on 12/11/17.
+//  Copyright © 2017 Manjit Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
